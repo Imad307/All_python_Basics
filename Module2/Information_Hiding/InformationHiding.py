3a5f272fafc53356da2767bdad76d7778443f949
Imad <76903345+Imad307@users.noreply.github.com>
2023-04-25 08:16:03 +0500

    Add files via upload

---
+++ Module2/Information_Hiding/InformationHiding.py
@@ -0,0 +1,14 @@
+'''->
+# Information hiding refers to the concept of hiding the inner workings of the classes and simply providing interface 
+# Through which outside world can communicate without knowing what is happening behind the scences within a class
+
+The purpose of information hiding is to implement classes in such a way that instances/objects should not be used for any 
+unauthorized access/change in classes. Also, the two classes cannot know about algorithms in other classes but can still communicate.
+'''
+
+'''
+Information Hiding has two components:
+1. Encapsulation
+2. Abstraction
+'''
+
