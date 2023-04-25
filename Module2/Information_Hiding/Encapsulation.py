0702ee4befff0a2be8c90fb010c99fd7e3aa2668
Imad <76903345+Imad307@users.noreply.github.com>
2023-04-25 08:16:55 +0500

    Add files via upload

---
+++ Module2/Information_Hiding/Encapsulation.py
@@ -0,0 +1,22 @@
+'''
+"Encapsulation in OOP refers to binding data and methods to manipulate data together in a single unit, that is, class."
+
+Depending upon this unit, objects are created. 
+Encapsulation is usually done to hide the state and representation of an object from outside. 
+A class can be thought of as a capsule having methods and properties inside it.
+
+
+When encapsulating classes, a good convention is to declare all variables of a class private.
+ This will restrict direct access by the code outside that class.
+
+At this point, a question can be raised: If the methods and variables are encapsulated in a class, 
+then how can they be used outside of that class?
+
+The answer to this is simple. One has to implement public methods to let the outside world communicate with this class. 
+These methods are called getters and setters. We can also implement other custom methods.
+
+Advantages of encapsulation
+Classes make the code easy to change and maintain.
+Properties to be hidden can be specified easily.
+We decide which outside classes or functions can access the class properties.
+'''
