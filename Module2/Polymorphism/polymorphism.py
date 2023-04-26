b15b3493ceabde697d63265d78e8545a0fd2081b
Imad <76903345+Imad307@users.noreply.github.com>
2023-04-26 09:20:58 +0500

    Add files via upload

---
+++ Module2/Polymorphism/polymorphism.py
@@ -0,0 +1,15 @@
+# PolyMorphism is a combination of two greek words: Poly means Many and Morph means Many. 
+'''
+In short polymorphism means that a thing which have many shapes and forms. 
+
+'''
+'''
+Consider an Example of Shape:
+Square, Rectangle, Circle, Polygon and a Diamond are examples of shape. 
+But Keep in mind that All of these shapes have different properties. This is Polymorphism. 
+'''
+'''
+Now consider if we have to code this example and we also have to calculate the area of each specific shape. 
+We have to mention a specific method for each subclass of shape. But Polymorphism allows us to use a single method getArea()
+in parent class instead of writing specific methods for each subclass. 
+'''
