a788a52ea2aa3c15b830d3fb8864aab33cfe94ba
Imad <76903345+Imad307@users.noreply.github.com>
2023-05-29 08:02:33 +0500

    Add files via upload

---
+++ Module_3/Data_Structure/Trees/Complete_BT.py
@@ -0,0 +1,9 @@
+'''
+A type of binary tree in which all nodes at all level are fully filled except for the last one and we fill the last level from left to right.
+Tot. no. of nodes in Complete binary tree: 2^h <= nodes <=  (2^(h+1))-1
+Tot. no of non-leaf nodes: 2^(h-1) <non-leaf nodes <=  (2^h)-1
+Tot. No. of leaf nodes: 2^(h-1)<leaf nodes<= 2^h
+Insertion in Complete Binary tree: 
+Nodes are inserted aree level by level.
+Fill in the far left nodes as possible. 
+'''
